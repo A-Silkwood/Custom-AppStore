0c08475df83ee17601f7a16168683a1b26a87ec9
Andrew Silkwood <andrewdavidsilkwood@gmail.com>
2020-10-15 12:56:23 -0700

    removed bug where that strings were actually arrays of strings

--- structs.h
+++ structs.h
@@ -1,16 +1,12 @@
 #ifndef MYAPPSTOREPROJECT_STRUCTS_H
     #include <iostream>
-    #define CAT_NAME_LEN 25
-    #define APP_NAME_LEN 50
-    #define VERSION_LEN 10
-    #define UNIT_SIZE 3
 
     struct AppInfo {
-        std::string category[CAT_NAME_LEN]; // app category
-        std::string name[APP_NAME_LEN]; // app name
-        std::string version[VERSION_LEN]; // version number
+        std::string category; // app category
+        std::string name; // app name
+        std::string version; // version number
         float size; // app size
-        std::string units[UNIT_SIZE]; // units for app size [GB/MB]
+        std::string units; // units for app size [GB/MB]
         float price; // app price
     };
 
@@ -21,12 +17,12 @@
     };
 
     struct Category {
-        std::basic_string<char> name; // category name
+        std::string name; // category name
         Tree *root; // binary search tree of app info
     };
 
     struct HashTableEntry {
-        std::string appName[APP_NAME_LEN]; // app name
+        std::string appName; // app name
         Tree *appNode; // node containing app info
         HashTableEntry *next; // next hash entry in chain
     };
